678d4ef23670f05edfd752539aa3cc03be1e8d57
atthaporncd <atthaporn@blockfint.com>
2023-03-13 13:33:00 +0700

    revoke access key & update gitignore

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,5 +1,12 @@
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
 provider "aws" {
     region = "ap-southeast-1"
-    access_key = "AKIAXCIMAOHKP46SQWFK"
-    secret_key = "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"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
 }
